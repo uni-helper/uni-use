0734eccc9393f3c84db06583cfff24739fd28620
ModyQyW <wurui-dev@foxmail.com>
2022-12-20 17:18:50 +0800

    fix: fix dts build error

--- src/useApp/index.ts
+++ src/useApp/index.ts
@@ -1,12 +1,10 @@
 import { ref } from 'vue';
-
-// FIXME
-// src/useApp/index.ts(4,17): error TS2742: The inferred type of 'useApp' cannot be named without a reference to '.pnpm/@dcloudio+types@3.2.3/node_modules/@dcloudio/types/uni-app/uni/base/UnhandledRejectiond'. This is likely not portable. A type annotation is necessary.
-// src/useApp/index.ts(4,17): error TS2742: The inferred type of 'useApp' cannot be named without a reference to '.pnpm/@dcloudio+types@3.2.3/node_modules/@dcloudio/types/uni-app/uni/base/ThemeChange'. This is likely not portable. A type annotation is necessary.
+// this line resolves dts build error
+import '@dcloudio/types/uni-app/app';
 
 /** Get the current application instance, use useGlobalData for globalData */
 export function useApp() {
-  const app = ref<ReturnType<typeof getApp>>(getApp());
+  const app = ref(getApp());
 
   return app;
 }
