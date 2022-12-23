f296e385b3ff8fc45f1dcdc9cf630121997da1b4
ModyQyW <wurui-dev@foxmail.com>
2022-12-23 14:17:34 +0800

    fix: fix build

--- src/useApp/index.ts
+++ src/useApp/index.ts
@@ -1,6 +1,4 @@
 import { ref } from 'vue';
-// this line resolves dts build error
-import '@dcloudio/types/uni-app/app';
 
 /** Get the current application instance, use useGlobalData for globalData */
 export function useApp() {
