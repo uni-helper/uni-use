6b980f1eccf6aae7585718c836c386ba440ae548
ModyQyW <wurui-dev@foxmail.com>
2023-02-15 17:24:37 +0800

    feat!: 调整 `useClipboardData` 实现，现在会返回可写计算属性

---
+++ src/useClipboardData/index.ts
@@ -0,0 +1,51 @@
+import { computed, ref } from 'vue';
+import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
+
+export type UseClipboardDataOptions = MaybeComputedRef<{
+  initialValue?: string;
+  showToast?: boolean;
+}>;
+
+/**
+ * 剪切板
+ *
+ * https://uniapp.dcloud.net.cn/api/system/clipboard.html
+ */
+export function useClipboardData(options: UseClipboardDataOptions = {}) {
+  const { initialValue } = resolveUnref(options);
+
+  const tempClipboardData = ref('');
+  function getClipboardData() {
+    uni.getClipboardData({
+      success: ({ data }) => {
+        tempClipboardData.value = data;
+      },
+    });
+  }
+  function setClipboardData(value: string) {
+    uni.setClipboardData({
+      data: value,
+      // 保证与保存的内容一致
+      success: () => getClipboardData(),
+    });
+  }
+
+  const clipboardData = computed({
+    get() {
+      return tempClipboardData.value;
+    },
+    set(value) {
+      setClipboardData(value);
+    },
+  });
+
+  // 如果没有传入初始值，直接获取当前剪切板内容
+  // 否则设置初始值并更新剪切板内容
+  if (initialValue === undefined) {
+    getClipboardData();
+  } else {
+    setClipboardData(initialValue);
+  }
+
+  return clipboardData;
+}
