40825301ce9f056ea60404465fa42a7dab4f1737
ModyQyW <wurui-dev@foxmail.com>
2023-02-15 12:59:07 +0800

    feat!: 调整 `useGlobalData` 实现，现在会返回可写计算属性

---
+++ src/useGlobalData/index.ts
@@ -0,0 +1,30 @@
+import { computed, ref } from 'vue';
+import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
+import type { AnyRecord } from '../types';
+
+export type UseGlobalDataOptions<T extends AnyRecord> = MaybeComputedRef<{
+  initialValue?: T;
+}>;
+
+/**
+ * globalData
+ *
+ * https://uniapp.dcloud.net.cn/collocation/App.html#globaldata
+ */
+export function useGlobalData<T extends AnyRecord>(options: UseGlobalDataOptions<T> = {}) {
+  const { initialValue = {} } = resolveUnref(options);
+
+  const app = ref(getApp());
+  const globalData = computed({
+    get() {
+      return app.value.globalData ?? {};
+    },
+    set(newValue) {
+      app.value.globalData = newValue;
+    },
+  });
+
+  globalData.value = initialValue;
+
+  return globalData;
+}
