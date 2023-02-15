f81ac2ac4ef1659b049d1a33fe3cf4564f1947f3
ModyQyW <wurui-dev@foxmail.com>
2023-02-15 10:54:48 +0800

    feat: update types and jsdoc

--- src/useActionSheet/index.ts
+++ src/useActionSheet/index.ts
@@ -1,15 +1,21 @@
 import { reactive } from 'vue';
 import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
 
-export interface UniShowActionSheetOptions
-  extends Omit<UniApp.ShowActionSheetOptions, 'title' | 'itemList'> {
+export interface UniShowActionSheetOptions extends Omit<UniApp.ShowActionSheetOptions, 'itemList'> {
+  /** 文字数组 */
   itemList: string[];
 }
 export type ShowActionSheetOptions = MaybeComputedRef<UniShowActionSheetOptions>;
+export type UseActionSheetOptions = ShowActionSheetOptions;
 
-export function useActionSheet(options?: ShowActionSheetOptions) {
-  const showActionSheet = (newOptions?: ShowActionSheetOptions) => {
-    uni.showActionSheet(
+/**
+ * 返回一个方法，调用后从底部向上弹出操作菜单
+ *
+ * https://uniapp.dcloud.net.cn/api/ui/prompt.html#showactionsheet
+ */
+export function useActionSheet(options?: UseActionSheetOptions) {
+  return function showActionSheet(newOptions?: ShowActionSheetOptions) {
+    return uni.showActionSheet(
       reactive({
         itemList: [],
         ...resolveUnref(options),
@@ -17,5 +23,4 @@ export function useActionSheet(options?: ShowActionSheetOptions) {
       }),
     );
   };
-  return showActionSheet;
 }
