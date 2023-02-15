dd7da82baf66358cacd91a27643083f31661d347
ModyQyW <wurui-dev@foxmail.com>
2023-02-15 11:55:20 +0800

    chore: add jsdoc

--- src/useProvider/index.ts
+++ src/useProvider/index.ts
@@ -3,10 +3,16 @@ import { reactive } from 'vue';
 
 export interface UniGetProviderOptions extends UniApp.GetProviderOptions {}
 export type GetProviderOptions = MaybeComputedRef<UniGetProviderOptions>;
+export type UseProviderOptions = GetProviderOptions;
 
-export function useProvider(options?: GetProviderOptions) {
-  const getProvider = (newOptions?: GetProviderOptions) => {
-    uni.getProvider(
+/**
+ * 返回一个方法，调用后获取服务供应商
+ *
+ * https://uniapp.dcloud.net.cn/api/plugins/provider?id=getprovider
+ */
+export function useProvider(options?: UseProviderOptions) {
+  return function getProvider(newOptions?: GetProviderOptions) {
+    return uni.getProvider(
       reactive({
         service: 'oauth',
         ...resolveUnref(options),
@@ -14,5 +20,4 @@ export function useProvider(options?: GetProviderOptions) {
       }),
     );
   };
-  return getProvider;
 }
