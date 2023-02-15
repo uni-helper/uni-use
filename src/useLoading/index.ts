3820348fc10f69765d1469b3c0d5df58d4171da9
ModyQyW <wurui-dev@foxmail.com>
2023-02-15 12:01:08 +0800

    feat!: 调整 `useLoading` 实现，现在会直接返回可调用的方法

--- src/useLoading/index.ts
+++ src/useLoading/index.ts
@@ -3,18 +3,37 @@ import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
 
 export interface UniShowLoadingOptions extends UniApp.ShowLoadingOptions {}
 export type ShowLoadingOptions = MaybeComputedRef<UniShowLoadingOptions>;
+export type UseLoadingOptions = ShowLoadingOptions;
 
-export function useLoading(options?: ShowLoadingOptions) {
-  const showLoading = (newOptions?: ShowLoadingOptions) => {
+/**
+ * 返回一个方法，调用后显示 loading 提示框
+ *
+ * https://uniapp.dcloud.net.cn/api/ui/prompt.html#showloading
+ */
+export function useLoading(options?: UseLoadingOptions) {
+  /**
+   * 显示 loading 提示框
+   *
+   * 返回一个方法，调用后隐藏 loading 提示框
+   *
+   * https://uniapp.dcloud.net.cn/api/ui/prompt.html#showloading
+   *
+   * https://uniapp.dcloud.net.cn/api/ui/prompt.html#hideloading
+   */
+  return function showLoading(newOptions?: ShowLoadingOptions) {
     uni.showLoading(
       reactive({
         ...resolveUnref(options),
         ...resolveUnref(newOptions),
       }),
     );
+    /**
+     * 隐藏 loading 提示框
+     *
+     * https://uniapp.dcloud.net.cn/api/ui/prompt.html#hideloading
+     */
+    return function hideLoading() {
+      return uni.hideLoading();
+    };
   };
-
-  const hideLoading = () => uni.hideLoading();
-
-  return { showLoading, hideLoading };
 }
