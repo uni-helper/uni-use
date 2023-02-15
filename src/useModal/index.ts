288ab2ccbb7b6c544431ba71e203e9f55864587f
ModyQyW <wurui-dev@foxmail.com>
2023-02-15 12:01:58 +0800

    chore: add jsdoc

--- src/useModal/index.ts
+++ src/useModal/index.ts
@@ -3,15 +3,25 @@ import { MaybeComputedRef, resolveUnref } from '@vueuse/core';
 
 export interface UniShowModalOptions extends UniApp.ShowModalOptions {}
 export type ShowModalOptions = MaybeComputedRef<UniShowModalOptions>;
+export type UseModalOptions = ShowModalOptions;
 
-export function useModal(options?: ShowModalOptions) {
-  const showModal = (newOptions?: ShowModalOptions) => {
-    uni.showModal(
+/**
+ * 返回一个方法，调用后显示模态弹窗
+ *
+ * https://uniapp.dcloud.net.cn/api/ui/prompt?id=showmodal
+ */
+export function useModal(options?: UseModalOptions) {
+  /**
+   * 显示模态弹窗
+   *
+   * https://uniapp.dcloud.net.cn/api/ui/prompt?id=showmodal
+   */
+  return function showModal(newOptions?: ShowModalOptions) {
+    return uni.showModal(
       reactive({
         ...resolveUnref(options),
         ...resolveUnref(newOptions),
       }),
     );
   };
-  return showModal;
 }
