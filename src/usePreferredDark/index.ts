0577690934ec8c9b31a60d1a33aa8b05ad2d13cc
ModyQyW <wurui-dev@foxmail.com>
2023-02-15 17:12:02 +0800

    chore: update jsdoc

---
+++ src/usePreferredDark/index.ts
@@ -0,0 +1,22 @@
+import { ref, readonly } from 'vue';
+import { tryOnScopeDispose } from '@vueuse/core';
+
+/**
+ * 响应式的暗黑主题偏好
+ *
+ * https://zh.uniapp.dcloud.io/api/system/theme.html
+ *
+ * https://uniapp.dcloud.net.cn/tutorial/darkmode.html
+ */
+export function usePreferredDark() {
+  const prefersDark = ref(uni.getSystemInfoSync().osTheme === 'dark');
+
+  const callback = ({ theme }: UniApp.OnThemeChangeCallbackResult) => {
+    prefersDark.value = theme === 'dark';
+  };
+  uni.onThemeChange(callback);
+  const stop = () => uni.offThemeChange(callback);
+  tryOnScopeDispose(stop);
+
+  return readonly(prefersDark);
+}
---
+++ src/usePreferredLanguage/index.ts
@@ -0,0 +1,28 @@
+import { tryOnScopeDispose } from '@vueuse/core';
+import { ref, readonly } from 'vue';
+
+/**
+ * 响应式的语言偏好
+ *
+ * https://uniapp.dcloud.net.cn/api/ui/locale.html
+ *
+ * https://uniapp.dcloud.net.cn/tutorial/i18n
+ */
+export function usePreferredLanguage() {
+  const locale = ref(uni.getLocale());
+
+  const callback = (result: UniApp.OnLocaleChangeCallbackResult) => {
+    locale.value = result.locale ?? locale.value;
+  };
+  uni.onLocaleChange(callback);
+  const stop = () => {
+    // @ts-expect-error no types
+    if (uni.offLocaleChange) {
+      // @ts-expect-error no types
+      uni.offLocaleChange(callback);
+    }
+  };
+  tryOnScopeDispose(stop);
+
+  return readonly(locale);
+}
