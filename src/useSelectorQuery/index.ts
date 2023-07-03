5dc991c4b598da923210dfccaadb589489ecb3b1
edwinhuish <edwinhuish@gmail.com>
2023-07-03 10:14:52 +0800

    feat: function useSelectorQuery

---
+++ src/useSelectorQuery/index.ts
@@ -0,0 +1,54 @@
+import { getCurrentInstance } from 'vue';
+
+export function useSelectorQuery() {
+  const instance = getCurrentInstance();
+  const query = uni.createSelectorQuery().in(instance);
+
+  function getNodeRef(selector: string | UniApp.NodesRef, all = false) {
+    return typeof selector === 'string'
+      ? all
+        ? query.selectAll(selector)
+        : query.select(selector)
+      : selector;
+  }
+
+  function getBoundingClientRect(selector: string | UniApp.NodesRef, all = false) {
+    return new Promise<UniApp.NodeInfo | UniApp.NodeInfo[]>((resolve) => {
+      getNodeRef(selector, all)
+        .boundingClientRect((rect) => resolve(rect))
+        .exec();
+    });
+  }
+
+  function getFields(selector: string | UniApp.NodesRef, fields: UniApp.NodeField, all = false) {
+    return new Promise<UniApp.NodeInfo | UniApp.NodeInfo[]>((resolve) => {
+      getNodeRef(selector, all)
+        .fields(fields, (data) => resolve(data))
+        .exec();
+    });
+  }
+
+  function getScrollOffset(node?: UniApp.NodesRef) {
+    return new Promise<UniApp.NodeInfo | UniApp.NodeInfo[]>((resolve) => {
+      node = node || query.selectViewport();
+      node.scrollOffset((res) => resolve(res)).exec();
+    });
+  }
+
+  function getContext(selector: string | UniApp.NodesRef, all = false) {
+    return new Promise<UniApp.NodeInfo | UniApp.NodeInfo[]>((resolve) => {
+      getNodeRef(selector, all)
+        .context((res) => resolve(res))
+        .exec();
+    });
+  }
+
+  return {
+    query,
+    getNodeRef,
+    getBoundingClientRect,
+    getFields,
+    getScrollOffset,
+    getContext,
+  };
+}
