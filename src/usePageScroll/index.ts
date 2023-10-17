2e9d9a41c75fbc77c6532bc134866b593483b42f
Jelf <okxiaoliang4@gmail.com>
2023-10-17 10:22:24 +0800

    feat(usePageScroll): 新增duration与scrollToSelector参数

--- src/usePageScroll/index.ts
+++ src/usePageScroll/index.ts
@@ -1,5 +1,6 @@
 import { onPageScroll } from '@dcloudio/uni-app';
-import { ref, computed } from 'vue';
+import { resolveUnref, watchWithFilter } from '@vueuse/core';
+import { ref, computed, MaybeRef } from 'vue';
 
 export interface UsePageScrollOptions {
   /**
@@ -9,6 +10,20 @@ export interface UsePageScrollOptions {
    * @see 因为 https://github.com/dcloudio/uni-app/issues/3099 所以默认值需要false，让页面被uniapp的正则捕获从而开启监听
    */
   onPageScroll?: boolean;
+
+  /**
+   * 滚动到指定选择器
+   *
+   * @see https://uniapp.dcloud.net.cn/api/ui/scroll?id=pagescrollto
+   */
+  scrollToSelector?: MaybeRef<string>;
+
+  /**
+   * 滚动动画时长
+   *
+   * @default 300
+   */
+  duration?: number;
 }
 
 /**
@@ -18,13 +33,18 @@ export interface UsePageScrollOptions {
  * @see https://uniapp.dcloud.net.cn/tutorial/page.html#onpagescroll
  */
 export default function usePageScroll(options: UsePageScrollOptions = {}) {
+  const { duration = 300, scrollToSelector } = options;
+
   const _scrollTop = ref(0);
   const scrollTop = computed({
     get() {
       return _scrollTop.value;
     },
     set(val) {
-      uni.pageScrollTo({ scrollTop: val });
+      uni.pageScrollTo({
+        scrollTop: val,
+        duration,
+      });
     },
   });
 
@@ -33,7 +53,21 @@ export default function usePageScroll(options: UsePageScrollOptions = {}) {
       _scrollTop.value = e.scrollTop;
     });
 
+  watchWithFilter(
+    () => resolveUnref(scrollToSelector),
+    (newValue) => {
+      uni.pageScrollTo({
+        selector: newValue,
+        duration,
+      });
+    },
+    {
+      eventFilter: (e) => e !== undefined,
+    },
+  );
+
   return {
     scrollTop,
+    scrollToSelector,
   };
 }
