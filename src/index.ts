25cd1311ed8603b74dcb5a26b7d742ae91a26b18
edwinhuish <edwinhuish@gmail.com>
2023-11-21 00:59:52 +0800

    style: simplify UniUseAutoImports package name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { name } from '../package.json';
 import * as UniUse from './exports';
 export * from './exports';
 
 export const UniUseAutoImports: Record<string, Array<string | [string, string]>> = {
-  '@uni-helper/uni-use': Object.keys(UniUse),
+  [name]: Object.keys(UniUse),
 };
