3791a436b57aaaa7f12054ed913754318a3e4242
edwinhuish <edwinhuish@gmail.com>
2024-01-17 11:38:25 +0800

    fix: deps build

--- build.config.ts
+++ build.config.ts
@@ -4,6 +4,9 @@ export default defineBuildConfig({
   entries: ['./src/index'],
   clean: true,
   declaration: true,
+  externals:[
+    '@dcloudio/uni-cli-shared',
+  ],
   rollup: {
     emitCJS: true,
     esbuild: {
