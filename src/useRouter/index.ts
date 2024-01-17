47bb547e4d04ae5bc8e0b43aad626587de8fbbbc
edwinhuish <edwinhuish@gmail.com>
2024-01-17 10:31:50 +0800

    feat: better useRouter

--- src/useRouter/index.ts
+++ src/useRouter/index.ts
@@ -1,20 +1,25 @@
 import { ref, computed } from 'vue';
+import { pathResolve } from 'src/utils';
+import type { AppJson } from '@dcloudio/uni-cli-shared'
+import type { RequiredOnly } from '../types'
 
 /** 获取当前页面栈信息 */
 const pages = ref(getCurrentPages());
 
 /** 获取当前页信息 */
 // at is not supported
-const currentPage = computed(() => pages.value?.[pages.value.length - 1]);
+const current = computed(() => pages.value?.[pages.value.length - 1]);
 /** 获取前一页信息 */
-const prevPage = computed(() =>
+const prev = computed(() =>
   pages.value.length > 1 ? pages.value[pages.value.length - 2] : pages.value?.[pages.value.length - 1],
 );
 
 /** 获取当前页路由信息 */
-const currentRoute = computed(() => currentPage.value?.route || '/');
+const currentUrl = computed(() => current.value?.route || '/');
 /** 获取前一页路由信息 */
-const prevRoute = computed(() => prevPage.value?.route);
+const prevUrl = computed(() => prev.value?.route);
+
+let tabBarList: TabBarItem[] = [];
 
 let isInited = false;
 
@@ -53,38 +58,36 @@ function warpPromiseOptions<T = any>(opts: T, resolve: (res: any) => any, reject
   };
 }
 
-function navigate(options: UniNamespace.NavigateToOptions): Promise<any> {
+/**
+ * 切换 tabbar 页面
+ */
+function switchTab(options: UniNamespace.SwitchTabOptions): Promise<any> {
   return new Promise((resolve, reject) => {
-    // 先尝试能否切换 tabbar
     uni.switchTab(
       warpPromiseOptions(options, resolve, reject),
     );
-  }).catch(() => {
-    // 失败再尝试跳转 navigateTo
-    return new Promise((resolve, reject) => {
-      uni.navigateTo(
-        warpPromiseOptions(options, resolve, reject),
-      );
-    });
-  });
+  })
 }
 
-function redirect(options: UniNamespace.RedirectToOptions): Promise<any> {
+function navigateTo(options: UniNamespace.NavigateToOptions) {
   return new Promise((resolve, reject) => {
-    // 先尝试能否切换 tabbar
-    uni.switchTab(
+    uni.navigateTo(
       warpPromiseOptions(options, resolve, reject),
     );
-  }).catch(() => {
-    // 失败再尝试跳转 redirectTo
-    return new Promise((resolve, reject) => {
-      uni.redirectTo(
-        warpPromiseOptions(options, resolve, reject),
-      );
-    });
   });
 }
 
+function redirectTo(options: UniNamespace.RedirectToOptions) {
+  return new Promise((resolve, reject) => {
+    uni.redirectTo(
+      warpPromiseOptions(options, resolve, reject),
+    );
+  });
+}
+
+/**
+ * 重定向，并清空当前页面栈
+ */
 function reLaunch(options: UniNamespace.ReLaunchOptions): Promise<any> {
   return new Promise((resolve, reject) => {
     uni.reLaunch(
@@ -93,6 +96,9 @@ function reLaunch(options: UniNamespace.ReLaunchOptions): Promise<any> {
   });
 }
 
+/**
+ * 后退
+ */
 function back(options?: UniNamespace.NavigateBackOptions): Promise<any> {
   return new Promise((resolve, reject) => {
     uni.navigateBack(
@@ -101,28 +107,114 @@ function back(options?: UniNamespace.NavigateBackOptions): Promise<any> {
   });
 }
 
-/** 获取路由信息 */
-export function useRouter() {
+/**
+ * 路由跳转
+ * `tryTabBar = true` 时，自动判断是否 tabbar 页面
+ */
+function trySwitchTab<FN extends (typeof navigateTo | typeof redirectTo)>(tryTabBar: boolean, forward: FN, options: Parameters<FN>[0]): Promise<any> {
+  // 不尝试 tabbar 页面，直接跳转
+  if (!tryTabBar) {
+    return forward(options);
+  }
+
+  // 未设置 tabBarList，先尝试 switchTab，报错再尝试跳转
+  if (!tabBarList.length) {
+    return switchTab(options).catch(() => navigateTo(options));
+  }
+
+  const url = typeof options.url === 'string' ? options.url : options.url.toString();
+
+  // 如果是 tabBar 页面，则直接 switchTab
+  if (isTabBarPath(url)) {
+    return switchTab(options);
+  }
+
+  // 不是 tabBar，直接跳转
+  return navigateTo(options);
+}
+
+
+function isTabBarPath(path: string) {
+  const target = pathResolve(path);
+  const tabbar = tabBarList.find(t => `/${t.pagePath}` === target);
+  return !!tabbar;
+}
+
+type UniTabBarItem = Exclude<AppJson['tabBar'], undefined>['list'][number]
+
+type TabBarItem = RequiredOnly<UniTabBarItem, 'pagePath'>;
+
+export interface UseRouterOptions {
+  /**
+   * 是否尝试跳转 tabBar
+   * 开启后，使用 navigate / redirect 将会先尝试 tabBar
+   * @default true
+   */
+  tryTabBar?: boolean;
+  /**
+   * pages.json 里的 tabBar list 配置
+   * tryTabBar 开启时，会判断跳转页面
+   * 全局配置，仅需要配置一次
+   */
+  tabBarList?: TabBarItem[];
+}
+
+
+/**
+ * 路由操作的封装
+ * 
+ * UNIAPP 官方文档 @see https://uniapp.dcloud.net.cn/api/router.html
+ */
+export function useRouter(options: UseRouterOptions = {}) {
   initIfNotInited();
+
+  const {
+    tryTabBar = true
+  } = options;
+
+  if (options.tabBarList) {
+    tabBarList = options.tabBarList;
+  }
+
+  /**
+   * 路由跳转
+   */
+  function navigate(options: UniNamespace.NavigateToOptions): Promise<any> {
+    return trySwitchTab(tryTabBar, navigateTo, options);
+  }
+
+  /**
+   * 路由重定向
+   */
+  function redirect(options: UniNamespace.RedirectToOptions): Promise<any> {
+    return trySwitchTab(tryTabBar, redirectTo, options);
+  }
+
 
   return {
     /** 获取当前页面栈信息 */
     pages,
     /** 获取当前页信息 */
-    currentPage,
-    /** @deprecated 弃用，请使用 currentPage */
-    page: currentPage,
+    current,
+    /** @deprecated 弃用，请使用 current */
+    page: current,
     /** 获取当前页路由信息 */
-    currentRoute,
-    /** @deprecated 弃用，请使用 currentRoute */
-    route: currentRoute,
+    currentUrl,
+    /** @deprecated 弃用，请使用 currentUrl */
+    route: currentUrl,
     /** 获取前一页信息 */
-    prevPage,
+    prev,
+    /** @deprecated 弃用，请使用 prev */
+    prevPage: prev,
     /** 获取前一页路由信息 */
-    prevRoute,
-    /** 路由跳转，如果是 tabbar 页面则执行 switchTab */
+    prevUrl,
+    /** @deprecated 弃用，请使用 prevUrl */
+    prevRoute: prevUrl,
+    /** 切换 tabbar 页面。 */
+    switchTab,
+    /** 路由跳转 */
     navigate,
-    /** 路由重定向，会替换当前页面，如果是 tabbar 页面则执行 switchTab */
+    /** 路由重定向 */
     redirect,
     /** 重定向，并清空当前页面栈 */
     reLaunch,
--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,15 @@ export const isFunction = <T extends (...args: any[]) => any>(val: any): val is 
   typeof val === 'function';
 
 export const noop = () => {};
+
+export function pathResolve(target: string, current?: string) {
+  if (!current) {
+    const pages = getCurrentPages();
+    current = pages[pages.length - 1].route;
+  }
+
+  if (!current) {
+    throw new Error('The current path is undefined and cannot be found.');
+  }
+  return new URL(target, new URL(current, 'http://no-exists.com')).pathname;
+}
