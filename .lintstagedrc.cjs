ab13812e9e5fba3c77ebefc96a0c962e34a9a245
edwinhuish <edwinhuish@gmail.com>
2024-01-17 17:30:44 +0800

    style: change auto import function name

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
   'package.json': 'sort-package-json',
   '*.md': 'markdownlint --fix --ignore-path=.gitignore',
-  './src/*.{js,cjs,mjs,ts,cts,mts}': 'pnpm run lint',
+  './src/*.{js,cjs,mjs,ts,cts,mts}': () => [
+    'tsc --noEmit',
+    'eslint --fix --cache --ignore-path=.gitignore',
+    'publint',
+  ],
 };
--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import { name } from '../package.json';
 import * as UniUse from './exports';
 export * from './exports';
 
-/** @deprecated 建议使用 `unpluginAutoImport` */
+/** @deprecated 建议使用 `uniuseAutoImports` 函数 */
 export const UniUseAutoImports: Record<string, Array<string | [string, string]>> = {
   [name]: Object.keys(UniUse),
 };
 
 export type UniUseFunction = keyof typeof UniUse;
 
-export interface UnpluginAutoImportOptions {
+export interface UniUseAutoImportsOptions {
   only?: UniUseFunction[];
   except?: UniUseFunction[];
 }
 
 /** 自定义配置 unplugin-auto-import */
-export function unpluginAutoImport(options: UnpluginAutoImportOptions = {}) {
+export function uniuseAutoImports(options: UniUseAutoImportsOptions = {}) {
   let exports = Object.keys(UniUse);
 
   if (options.only) {
