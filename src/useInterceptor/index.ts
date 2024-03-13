dc3eba5d8eddd0c8130eac9d2cce7c6dec4cc9cd
edwinhuish <edwinhuish@gmail.com>
2024-03-13 11:32:30 +0800

    feat: 强化 useInterceptor，支持 sync 函数

--- src/useInterceptor/index.ts
+++ src/useInterceptor/index.ts
@@ -1,15 +1,79 @@
-import { tryOnScopeDispose } from '@vueuse/core';
+import { invoke } from '@vueuse/core';
+import { tryOnScopeDispose } from '../tryOnScopeDispose';
+
+type FunctionKeys<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never;
+}[keyof T];
+
+type UniFunctions = FunctionKeys<Uni>;
+
+export interface InterceptorOptions extends UniApp.InterceptorOptions {
+  /** 返回 false 则终止执行 */
+  invoke?: (args: any) => void | boolean;
+}
 
 /**
  * 注册拦截器，在活跃的 effect 作用域停止时自动移除
  *
- * https://cn.vuejs.org/api/reactivity-advanced.html#effectscope
+ * https://cn.vuejs.org/api/reactivity-advanced.htmlSeffectscope
  */
-export function useInterceptor(event: string, options: UniApp.InterceptorOptions) {
-  uni.addInterceptor(event, options);
+export function useInterceptor(event: UniFunctions, options: InterceptorOptions, autoRemove = true) {
+  const origin = uni[event] as Function;
 
-  const stop = () => uni.removeInterceptor(event);
-  tryOnScopeDispose(stop);
+  uni[event] = (...args: any) => {
+    let result: any;
+
+    let skip = false;
+
+    try {
+      /**
+       * 拦截前触发
+       */
+      if (options.invoke && options.invoke(args) === false) {
+        console.log(`${event} invoke return FALSE, SKIP IT`);
+        skip = true;
+        // 如果返回false，则终止执行
+        return;
+      }
+
+      /**
+       * 调用原始方法
+       */
+      result = origin(...args);
+      /**
+       * 方法调用后触发，处理返回值
+       */
+      options.returnValue && options.returnValue(result);
+      /**
+       * 成功回调拦截
+       */
+      options.success && options.success(result);
+
+      return result;
+    }
+    catch (err: any) {
+      /**
+       * 失败回调拦截
+       */
+      options.fail && options.fail(err);
+
+      throw err;
+    }
+    finally {
+      /**
+       * 完成回调拦截
+       */
+      !skip && options.complete && options.complete(result);
+    }
+  };
+
+  const stop = () => {
+    uni[event] = origin as any;
+  };
+
+  if (autoRemove) {
+    tryOnScopeDispose(stop);
+  }
 
   return stop;
 }
