a129dabbd44321697873fa2fa6bb359402971189
Chang <AresChang114@qq.com>
2024-10-12 15:37:19 +0800

    chore: config vitepress docs

---
+++ src/.vitepress/config.ts
@@ -0,0 +1,46 @@
+import fg from 'fast-glob';
+import { defineConfig } from 'vitepress';
+
+const files = await fg('*', {
+  onlyDirectories: true,
+  cwd: './src',
+  ignore: [
+    'public',
+    'api',
+  ],
+});
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'UniUse',
+  description: 'uni-app (vue3) 组合式工具集。',
+  lang: 'zh-CN',
+  themeConfig: {
+    logo: '/logo.png',
+
+    nav: [
+      { text: '指南', link: '/' },
+      { text: 'API', link: '/api' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Api',
+        items: [
+          {
+            text: '函数列表',
+            link: '/api',
+          },
+          ...files.map(file => ({
+            text: file,
+            link: `/${file}/readme.md`,
+          })),
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/uni-helper/uni-use' },
+    ],
+  },
+});
---
+++ src/index.md
@@ -0,0 +1,26 @@
+---
+layout: home
+
+hero:
+  name: UniUse
+  text: uni-app 组合式工具集
+  tagline: 为 uni-app 量身打造的组合式工具集
+  image:
+    src: /logo.png
+    alt: UniUse
+  actions:
+    - theme: brand
+      text: 开始
+      link: /start
+    - theme: alt
+      text: API 集合
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
