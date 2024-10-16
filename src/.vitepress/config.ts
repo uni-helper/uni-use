12fcd86b70e07502ad264f0b1622d2a8cf46f6b9
Chang <AresChang114@qq.com>
2024-10-16 16:11:12 +0800

    docs: add document prompt configuration

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -18,14 +18,6 @@ export default defineConfig({
   lang: 'zh-CN',
   themeConfig: {
     logo: '/logo.png',
-    editLink: {
-      pattern: 'https://github.com/uni-helper/uni-use/tree/main/src/:path',
-      text: '为这个页面提供建议',
-    },
-    footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022-PRESENT uni-helper and uni-helper contributors',
-    },
 
     nav: [
       { text: '指南', link: '/guide/' },
@@ -76,6 +68,65 @@ export default defineConfig({
         },
       ],
     },
+
+    editLink: {
+      pattern: 'https://github.com/uni-helper/uni-use/tree/main/src/:path',
+      text: '为这个页面提供建议',
+    },
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-PRESENT uni-helper and uni-helper contributors',
+    },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+
+    outline: {
+      label: '页面导航',
+    },
+
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          'zh-CN': {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
+
+    langMenuLabel: '多语言',
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/uni-helper/uni-use' },
